cd59fde1d5468cbdef7a9c11b370458cc227957c
anoff <offenhaeuser@gmail.com>
2017-10-24 21:13:15 +0200

    :star2: feat: replicate lesson sudoku behavior

--- solution.py
+++ solution.py
@@ -29,7 +29,7 @@ def naked_twins(values):
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [a+b for a in A for b in B]
 
 def grid_values(grid):
     """
@@ -39,9 +39,15 @@ def grid_values(grid):
     Returns:
         A grid in dictionary form
             Keys: The boxes, e.g., 'A1'
-            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+            Values: The value in each box, e.g., '8'. If the box has no value,
+                then the value will be '123456789'.
     """
-    pass
+    boxes = cross("ABCDFEGHI", "123456789")
+    d = dict(zip(boxes, grid))
+    for k, v in d.items():
+        if v == ".":
+            d[k] = "123456789"
+    return d
 
 def display(values):
     """
@@ -51,17 +57,89 @@ def display(values):
     """
     pass
 
-def eliminate(values):
-    pass
+def unitlist():
+    """
+    return list of units that belong together
+    """
+    rows = 'ABCDEFGHI'
+    cols = '123456789'
+    row_units = [cross(r, cols) for r in rows]
+    column_units = [cross(rows, c) for c in cols]
+    square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+    return row_units + column_units + square_units
 
-def only_choice(values):
-    pass
+def peers():
+    """
+    return peers dictionary
+    taken from lesson 🙄
+    """
+    rows = 'ABCDEFGHI'
+    cols = '123456789'
+    boxes = cross(rows, cols)
+    units = dict((s, [u for u in unitlist() if s in u]) for s in boxes)
+    return dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
+
+def narrow_down_possibilities(values):
+    """
+    find boxes with only one value
+    delete this from all it's peers
+    """
+    for box in [b for b in values.keys() if len(values[b]) == 1]:
+        digit = values[box]
+        for peer in peers()[box]:
+            values = assign_value(values, box, values[peer].replace(digit, ""))
+    return values
+
+def assign_explicits(values):
+    """
+    if there is only one possible position for a value in a unit -> assign it
+    """
+    for unit in unitlist():
+        for number in "123456789":
+            box_with_number = [box for box in unit if number in values[box]]
+            if len(box_with_number) == 1:
+                values = assign_value(values, box_with_number[0], number)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    iterate over the puzzle until no boxes change
+    if completed or stalled return the list of values
+    if unsolvable (box without values) return False
+    """
+    def count_solved_boxes(values):
+        return len([b for b in values.keys() if len(values[b] == 1)])
+    improved = True
+    while improved:
+        improved = False
+        previous = values
+        values = assign_explicits(values)
+        values = narrow_down_possibilities(values)
+        if len([b for b in values.keys() if len(values[b]) == 0]) > 0:
+            return False
+        elif count_solved_boxes(previous) != count_solved_boxes(values):
+            improved = True
+    return values
 
 def search(values):
-    pass
+    """
+    recursively solve possible variations by assuming values in inexplicit boxes
+    return False if unsolvable
+    return values if solved
+    """
+    values = reduce_puzzle(values)
+    if values == False:
+        return False
+    if len([b for b in values.keys() if len(values[b] > 1)]) == 0:
+        return values
+    # find an unsolved (>1) box with the least amount of possible digits
+    _, box = min([(len(values[b]), b) for b in values.keys() if len(values[b]) > 1])
+    for digit in values[box]:
+        sudoku_star = values.copy()
+        sudoku_star[box] = digit
+        result = search(sudoku_star)
+        if result: # not False (= unsolvable)
+            return result
 
 def solve(grid):
     """
